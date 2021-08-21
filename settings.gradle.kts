546ab59f2f037cafc513c4bbdbe420411ad06609
MaxWayne <pikselnsk@gmail.com>
2021-08-21 22:41:25 +0700

    added open api spec

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,15 @@ rootProject.name = "Enotty"
 pluginManagement {
     plugins {
         val kotlinVersion: String by settings
+        val openApiVersion: String by settings
+
         kotlin("jvm") version kotlinVersion
         kotlin("multiplatform") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+
+        id("org.openapi.generator") version openApiVersion
+
     }
 }
 include("common")
+include("enotty-be-transport-open-api")
