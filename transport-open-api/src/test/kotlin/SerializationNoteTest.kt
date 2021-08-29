4d53ba0fadd5e69644c8d7a5c2215c119ec1510c
MaxWayne <pikselnsk@gmail.com>
2021-08-29 15:41:12 +0700

    added mappers test

--- transport-open-api/src/test/kotlin/SerializationNoteTest.kt
+++ transport-open-api/src/test/kotlin/SerializationNoteTest.kt
@@ -4,6 +4,7 @@ import info.javaway.android.enotty.openapi.models.CreatableNote
 import info.javaway.android.enotty.openapi.models.CreateNoteRequest
 import info.javaway.android.enotty.openapi.models.ResponseNote
 import org.junit.Test
+import java.util.*
 import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
@@ -24,8 +25,8 @@ class SerializationNoteTest {
                     extendedMode = false,
                     icon = "",
                     showTitle = true,
-                    createdAt = "",
-                    updatedAt = "",
+                    createdAt = Date().time.toBigDecimal(),
+                    updatedAt = Date().time.toBigDecimal(),
                     userUid = "",
             )
     )
